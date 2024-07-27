bed31d752e8ad05a17457cebf44be562541de506
Ian P. Mostar <ianmostar@gmail.com>
2024-07-28 01:01:50 +0800

    refactor: update hero image

--- src/app/shared/components/mobile-nav/mobile-nav.component.ts
+++ src/app/shared/components/mobile-nav/mobile-nav.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./mobile-nav.component.scss']
 })
 export class MobileNavComponent {
+  name = "Init text";
+  count = 0;
+  errorMsg = "";
 
+  onNameChange(ev: any) {
+    this.name = ev.target.value
+  }
+
+  validate() {
+    this.errorMsg = "A maximum of 10 orders is only allowed."
+  }
+
+  incrementCount() {
+    this.count++;
+  }
+
+  decrementCount() {
+    if (this.count <= 0) {
+      this.count = 0;
+      return;
+    }
+    this.count--;
+  }
 }

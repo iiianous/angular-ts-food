ddd797e1f3f8288564480ada06c0a738e5f7bc10
Ian P. Mostar <ianmostar@gmail.com>
2024-09-16 22:41:10 +0800

    chore: add layout width config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        1024: "64rem",
+        1200: "75rem",
+        1280: "80rem",
+        1536: "96rem",
+      },
+    },
   },
   plugins: [],
-  purge: [
-    './src/**/*.html',
-    './src/**/*.ts',
-  ],
-}
-
+  purge: ["./src/**/*.html", "./src/**/*.ts"],
+};
